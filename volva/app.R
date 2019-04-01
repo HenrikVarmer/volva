e8140a04288bdcc9a6e56db511e7fe80827ced0d
Henrik Varmer <you@example.com>
2019-04-01 21:19:42 +0200

    d

--- volva/app.R
+++ volva/app.R
@@ -15,6 +15,10 @@ ui <- fluidPage(
     # Sidebar panel for inputs ----
     sidebarPanel(
       
+      strong("Upload a .csv file. File must contain two columns."),
+      p("The first column must be a date value in the format YYYY-MM-DD."),
+      p("The second column must be a numeric or integer value containing the values you wish to forecast."),
+      
       # Input: Select a file ----
       fileInput("datafile", "Choose CSV File",
                 multiple = FALSE,
@@ -62,7 +66,8 @@ ui <- fluidPage(
       # Output: Tabset w/ plot, summary, and table ----
       tabsetPanel(type = "tabs",
                   tabPanel("Table", tableOutput("contents")),
-                  tabPanel("Plot", plotOutput("plot"))
+                  tabPanel("Plot", plotOutput("plot")),
+                  tabPanel("Predictions", tableOutput("prediction"))
                   
       )
       
@@ -147,7 +152,56 @@ server <- function(input, output) {
     
     })
     })
+   
+  
+  output$prediction <- renderTable({
     
+    withProgress(message = 'Creating plot', style = "notification", value = 0.1, {
+      
+      df <- read.csv(input$datafile$datapath,
+                     header = input$header,
+                     sep = input$sep,
+                     quote = input$quote)
+      
+      df[,1] <- as.character(df[,1])
+      df[,2] <- as.integer(df[,2])
+      
+      incProgress(0.25)
+      
+      #some required data cleaning
+      colnames(df) <- c("ds", "y")
+      
+      df$ds <- as.Date(df[,1], format = "%Y-%m-%d")
+      
+      df <- df %>% 
+        select(ds, y)
+      
+      #Fit the prophet model to input data
+      m <- prophet(df)
+      
+      #Create a dataframe for the future
+      future <- make_future_dataframe(m, periods = 365)
+      
+      
+      
+      incProgress(0.5)
+      
+      #Make a future prediction based on the fitted model
+      forecast <- predict(m, future)
+      
+      forecast <- as.data.frame(forecast[c('ds', 'yhat', 'yhat_lower', 'yhat_upper')])
+      
+      forecast$ds <- date(forecast$ds)
+      
+      return(forecast)
+      
+      
+      
+      incProgress(0.75)
+      incProgress(1)
+      
+    })
+  }) 
   
 }
 
